b4e3005c1cf6ee84c42d7a05674c0ca92fc9e455
eazytbay <eazytbay@gmail.com>
2024-07-31 15:29:32 +0100

    0-nqueens.py updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,64 +1,91 @@
 #!/usr/bin/python3
-"""The Backtracking algorithm solution for NQueens task"""
+'''Backtrading N Queens Challenge'''
 
 import sys
 
 
-class Nqueens:
-    """Definition of A class containing the solution to the Nqueen problem"""
-    def SolveNqueens(self, n):
-        """The solution method and all the core data and
-        algorithms"""
-        column = []
-        positiveDiagonal = set()  # row + column is constant
-        negativeDiagonal = set()  # row - column is constant
-        row = []
-
-        res = []
-
-        def check_validation(x, y):
-            """Checks if the current position is valid to place a queen"""
-            return y not in column and (x + y) not in positiveDiagonal \
-                and (x - y) not in negativeDiagonal
-
-        def bktrack(x):
-            """This is a backtracking algo that validates if
-            the queen is already on that given row and
-            positiveDiagonal or negativeDiagonal then
-            discarding that position recursively"""
-            if x == n:
-                res.append([[row[x], column[x]] for x in range(n)])
-                return
-
-            for y in range(n):
-                if check_validation(x, y):
-                    column.append(y)
-                    row.append(x)
-                    positiveDiagonal.add(x + y)
-                    negativeDiagonal.add(x - y)
-
-                    bktrack(x + 1)
-
-                    column.pop()
-                    row.pop()
-                    positiveDiagonal.remove(x + y)
-                    negativeDiagonal.remove(x - y)
-
-        bktrack(0)
-
-        for solution in res:
-            print(solution)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    solution = Nqueens()
-    if not sys.argv[1].isnumeric():
-        print("N must be a number")
-        sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    solution.SolveNqueens(int(sys.argv[1]))
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    x = 0
+    y = 0
+
+    # iterate thru rows
+    while x < n:
+        goback = False
+        # iterate thru columns
+        while y < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == y or col + (x-cord[0]) == y or
+                        col - (x-cord[0]) == y):
+                    safe = False
+                    break
+
+            if not safe:
+                if y == n - 1:
+                    goback = True
+                    break
+                y += 1
+                continue
+
+            # place queen
+            cords = [x, y]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if x == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        x = cord[0]
+                        y = cord[1]
+                for z in range(n - x):
+                    placed_queens.pop()
+                if x == n - 1 and y == n - 1:
+                    placed_queens = []
+                    stop = True
+                x -= 1
+                y += 1
+            else:
+                y = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            x -= 1
+            while x >= 0:
+                y = placed_queens[x][1] + 1
+                del placed_queens[x]  # delete previous queen coordinates
+                if y < n:
+                    break
+                x -= 1
+            if x < 0:
+                break
+            continue
+        x += 1
+
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
